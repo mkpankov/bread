c36ff012d8761471ac43736b020d2d4ce6888cd9
Michael Pankov <work@michaelpankov.com>
2014-12-01 20:59:51 +0300

    Add simplest test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+fn prepare(s: &str) -> String {
+    s.to_string()
+}
+
+
+// red   '\033[0;31m'
+// reset '\033[0m'
 #[test]
-fn it_works() {
+fn fg_two_colors() {
+    let input = "^fg(red)I'm red text ^fg(blue)I am blue";
+    assert!(prepare(input).as_slice() == "\033[0;31mI'm red text\033[0;34mI am blue\033[0m")
 }
