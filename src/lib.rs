fd2f696fe69419e7ba4a5e67f9776b395efc733c
Michael Pankov <work@michaelpankov.com>
2014-12-03 23:25:42 +0300

    Refactor to separate function

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+fn prepare_folder(a: String, b: char) -> String {
+    match b {
+        '.' => [a.as_slice(), ":"].concat(),
+        _ => [a, String::from_char(1, b)].concat(),
+    }
+}
+
 pub fn prepare(s: String) -> String {
-    s.chars().fold("".to_string(), |a, b|
-                   match b {
-                       '.' => [a.as_slice(), ":"].concat(),
-                       _ => [a, String::from_char(1, b)].concat(),
-                   }
-                   )
+    s.chars().fold("".to_string(),
+                   prepare_folder)
 }
 
 
@@ -13,5 +16,6 @@ pub fn prepare(s: String) -> String {
 #[test]
 fn fg_two_colors() {
     let input = "^fg(red)I'm red text ^fg(blue)I am blue";
-    assert!(prepare(input.into_string()).as_slice() == "\033[0;31mI'm red text\033[0;34mI am blue\033[0m")
+    assert!(prepare(input.into_string()).as_slice()
+         == "\033[0;31mI'm red text\033[0;34mI am blue\033[0m")
 }
