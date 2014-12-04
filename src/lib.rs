7d76f9eb2233bcd1a395414b72f02c1c751afd2b
Michael Pankov <work@michaelpankov.com>
2014-12-04 21:50:20 +0300

    Attempt to implement folding parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,66 @@
-fn prepare_folder(a: String, b: char) -> String {
-    match b {
-        '.' => [a.as_slice(), ":"].concat(),
-        _ => [a, String::from_char(1, b)].concat(),
-    }
+use List::{Next, End};
+use Expression::{Literal, Foreground};
+
+#[deriving (Show)]
+enum Color {
+    Red,
+    Blue,
+}
+
+#[deriving (Show)]
+enum Expression {
+    Literal(String),
+    Foreground(Color),
+}
+
+#[deriving (Show)]
+enum List {
+    Next(Expression, Box<List>),
+    End
+}
+
+#[deriving (Show)]
+struct Parser {
+    sentence: List,
+    current: Option<Expression>,
+}
+
+fn prepare_folder(a: &mut Parser, b: char) -> Option<Parser> {
+    let r = match b {
+        _ => match *a {
+            Parser { ref sentence, current: ref maybe_current } => {
+                match maybe_current {
+                    &None =>
+                        Parser { sentence: *sentence,
+                                 current: Some(Literal(String::from_char(1, b))) },
+                    &Some(current) =>
+                        match current {
+                            Literal(string) =>
+                                Parser { sentence: *sentence,
+                                         current: Some(Literal([string, String::from_char(1, b)].concat())) },
+                            Foreground(color) =>
+                                Parser { sentence:
+                                         match sentence {
+                                             &Next(h, e) => Next(h, box Next(current, e)),
+                                             &End => Next(current, box End),
+                                         },
+                                         current: Some(Literal(String::from_char(1, b))) }
+                        }
+                }
+            }
+        }
+    };
+    Some(r)
 }
 
 pub fn prepare(s: String) -> String {
-    s.chars().fold("".to_string(),
-                   prepare_folder)
+    let mut r;
+    for i in s.chars().scan(Parser { sentence: End, current: None},
+                            prepare_folder) {
+        println!("{}", i);
+        r = i;
+    }
+    format!("{}", r)
 }
 
 
