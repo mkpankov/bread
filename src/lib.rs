63f28d235537b68065cbe2b866bbdcedf60a802b
Michael Pankov <work@michaelpankov.com>
2014-12-05 23:55:06 +0300

    Remake parsing w/o fancy folding

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,48 @@
-use List::{Next, End};
-use Expression::{Literal, Foreground};
+use State::{Beginning, Opening, Inside, Closing, Literal};
 
-#[deriving (Show)]
-enum Color {
-    Red,
-    Blue,
+enum State {
+    Beginning,
+    Opening,
+    Inside,
+    Closing,
+    Literal,
 }
 
-#[deriving (Show)]
-enum Expression {
-    Literal(String),
-    Foreground(Color),
-}
-
-#[deriving (Show)]
-enum List {
-    Next(Expression, Box<List>),
-    End
-}
-
-#[deriving (Show)]
-struct Parser {
-    sentence: List,
-    current: Option<Expression>,
-}
-
-fn prepare_folder(a: &mut Parser, b: char) -> Option<Parser> {
-    let r = match b {
-        _ => match *a {
-            Parser { ref sentence, current: ref maybe_current } => {
-                match maybe_current {
-                    &None =>
-                        Parser { sentence: *sentence,
-                                 current: Some(Literal(String::from_char(1, b))) },
-                    &Some(current) =>
-                        match current {
-                            Literal(string) =>
-                                Parser { sentence: *sentence,
-                                         current: Some(Literal([string, String::from_char(1, b)].concat())) },
-                            Foreground(color) =>
-                                Parser { sentence:
-                                         match sentence {
-                                             &Next(h, e) => Next(h, box Next(current, e)),
-                                             &End => Next(current, box End),
-                                         },
-                                         current: Some(Literal(String::from_char(1, b))) }
-                        }
-                }
-            }
-        }
-    };
-    Some(r)
-}
-
-pub fn prepare(s: String) -> String {
+pub fn prepare(s: String) -> Result<String, String> {
     let mut r;
-    for i in s.chars().scan(Parser { sentence: End, current: None},
-                            prepare_folder) {
-        println!("{}", i);
-        r = i;
+    let mut state = Beginning;
+    let mut current = String::new();
+    let mut tokens = vec![];
+    for i in s.chars() {
+        match state {
+            Beginning => match i {
+                '^' => {
+                    state = Opening;
+                    tokens.push(current);
+                },
+                _   => {
+                    state = Literal;
+                    current.grow(1, i);
+                },
+            },
+            Opening => match i {
+                '(' => {
+                    state = Inside;
+                },
+                  _ => return Err(format!("Expected '(', found {}", i)),
+                },
+            Inside => match i {
+                ')' => {
+                    state = Closing;
+                    tokens.push(current);
+                }
+                  _ => {
+                    state = Inside;
+                    current.grow(1, i);
+                },
+            },
+        }
     }
-    format!("{}", r)
 }
 
 
@@ -69,6 +51,6 @@ pub fn prepare(s: String) -> String {
 #[test]
 fn fg_two_colors() {
     let input = "^fg(red)I'm red text ^fg(blue)I am blue";
-    assert!(prepare(input.into_string()).as_slice()
+    assert!(prepare(input.into_string()).unwrap().as_slice()
          == "\033[0;31mI'm red text\033[0;34mI am blue\033[0m")
 }
