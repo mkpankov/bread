5635300b494e3bb2eb5005eb1fa630e3fd807fdf
Michael Pankov <work@michaelpankov.com>
2014-12-07 16:47:21 +0300

    Use enum for colors

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 use State::{Beginning, Tag, Inside};
 use Token::{Fg, Literal};
 
+#[deriving(Show, PartialEq, Eq)]
+enum Color {
+    Red,
+    Blue,
+}
+
 #[deriving(Show)]
 enum State {
     Beginning,
@@ -10,7 +16,7 @@ enum State {
 
 #[deriving(Show, PartialEq, Eq)]
 enum Token {
-    Fg(String),
+    Fg(Color),
     Literal(String),
 }
 
@@ -66,7 +72,12 @@ fn parse(s: String) -> Result<Vec<Token>, String> {
                             match *i {
                                 ')' => {
                                     state = Beginning;
-                                    tokens.push(Fg(current.clone()));
+                                    let color = match current.as_slice() {
+                                        "red" => Color::Red,
+                                        "blue" => Color::Blue,
+                                        _ => return Err(format!("Expected color name, found {}", current)),
+                                    };
+                                    tokens.push(Fg(color));
                                     current = String::new();
                                 },
                                 _ => {
@@ -83,6 +94,10 @@ fn parse(s: String) -> Result<Vec<Token>, String> {
     Ok(tokens)
 }
 
+fn render(tokens: Vec<Token>) -> Result<String, String> {
+    Ok("".into_string())
+}
+
 pub fn prepare(s: String) -> Result<String, String> {
     Ok("".into_string())
 }
@@ -94,9 +109,9 @@ fn parse_fg_two_colors() {
     assert!(parse(input.into_string())
          == Ok(
              vec![Literal("".into_string()),
-              Fg("red".into_string()),
+              Fg(Color::Red),
               Literal("I'm red text ".into_string()),
-              Fg("blue".into_string()),
+              Fg(Color::Blue),
               Literal("I am blue".into_string())]))
 
 }
