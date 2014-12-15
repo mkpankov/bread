0c74c90854ac51d73023b2d504330e7a94fa309c
Michael Pankov <work@michaelpankov.com>
2014-12-15 21:11:10 +0300

    Remove dead code

--- src/test.rs
+++ src/test.rs
@@ -4,7 +4,6 @@ extern crate term;
 use bread::render;
 
 fn main() {
-    //let input = "^fg(red)I'm red text. ^fg(blue)I am blue.";
     let input = "^fg(bright-green)^bg(blue)I'm bright green \
                  ^fg(bright-magenta)^bg(bright-black)^bold()I am bright magenta \
                  ^reset()^dim()I'm just dim \
