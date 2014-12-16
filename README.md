503db36b27074baba591abc5ecb0ac50ffc50647
Michael Pankov <work@michaelpankov.com>
2014-12-16 21:30:28 +0300

    Improve example and README, add demo

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # bread [![Build Status](https://travis-ci.org/mkpankov/bread.svg?branch=master)](https://travis-ci.org/mkpankov/bread)
 
-Text formatting library for terminal output.
+Text formatting library for terminal output, with embedded formatting tokens.
 
 We're going to use formatting syntax of Dzen (https://github.com/robm/dzen). Some examples:
 
 * `^fg(red)I'm red text ^fg(blue)I am blue`
 * `^bg(red)The ^fg(bright-black)text to ^bg(blue)^fg(cyan)colorize`
+
+For full example of working program, see `src/test.rs`. You can run it by cloning the repo and doing `cargo run`.
+
+To use in your project, add
+
+```
+[dependencies.bread]
+
+git = "https://github.com/mkpankov/bread.git"
+
+```
+
+to `Cargo.toml` and you should be good to go.
 
 ## List of currently supported colors
 
@@ -25,3 +38,7 @@ We're going to use formatting syntax of Dzen (https://github.com/robm/dzen). Som
 * red
 * white
 * yellow
+
+## Demo
+
+[![Demo](https://raw.githubusercontent.com/mkpankov/bread/master/show.gif)](https://github.com/mkpankov/bread)
--- src/test.rs
+++ src/test.rs
@@ -6,23 +6,25 @@ use bread as B;
 use bread::{render_str, render};
 
 fn main() {
-    let input = "^fg(bright-green)^bg(blue)I'm bright green \
-                 ^fg(bright-magenta)^bg(bright-black)^bold()I am bright magenta \
-                 ^reset()^dim()I'm just dim \
-                 ^reset()^italic(true)I'm italic ^italic(false)I'm not \
-                 ^reset()^underline(true)I'm underlined^underline(false)I'm not \
-                 ^reset()^blink()I blink \
-                 ^reset()^standout(true)I stand out^standout(false)I don't \
-                 ^reset()^reverse()I'm reversed^reset()I'm not \
-                 ^reset()^secure()I'm secure^reset()\
-                 \n";
+    let input =
+        "^fg(bright-green)^bg(blue)I'm bright green.\n\
+         ^fg(bright-magenta)^bg(bright-black)^bold()\
+         I am bold bright magenta on bright-black background.\n\
+         ^reset()^dim()I'm just dim\n\
+         ^reset()^italic(true)I'm italic. ^italic(false)I'm not.\n\
+         ^reset()^underline(true)I'm underlined. ^underline(false)I'm not.\n\
+         ^reset()^blink()I blink\n\
+         ^reset()^standout(true)I stand out. ^standout(false)I don't.\n\
+         ^reset()^reverse()I'm reversed. ^reset()I'm not.\n\
+         ^reset()^secure()I'm secure.^reset()\n\
+         \n";
     let mut t = term::stdout();
     match t {
         None => panic!("Couldn't get terminal"),
         Some(ref mut t) => render_str(t, input).unwrap(),
     };
 
-    let tokens = [T::Fg(Some(B::RED)), T::Literal(format!("I'm red")), T::Reset];
+    let tokens = [T::Fg(Some(B::RED)), T::Literal(format!("I'm red\n")), T::Reset];
     match t {
         None => panic!("Couldn't get terminal"),
         Some(ref mut t) => render(t, &tokens),
